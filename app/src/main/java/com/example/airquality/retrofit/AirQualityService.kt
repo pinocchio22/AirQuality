183e195e56fcf66547cce635cf86bba132ab653f
pinocchio22 <vviian.2@gmail.com>
2022-06-09 17:55:44 +0900

    AirQualityService

---
+++ app/src/main/java/com/example/airquality/retrofit/AirQualityService.kt
@@ -0,0 +1,16 @@
+package com.example.airquality.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2022-06-09
+ * @desc
+ */
+interface AirQualityService {
+    @GET("nearest_city")
+    fun getAirQualityData(@Query("lat") lat : String, @Query("lon") lon : String, @Query("key") key : String) : Call<AirQualityResponse>
+}
