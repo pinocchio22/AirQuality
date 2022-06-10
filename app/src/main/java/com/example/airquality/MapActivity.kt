13b5f8d0e11ecbb95e02f8e273d899b073f6e270
pinocchio22 <vviian.2@gmail.com>
2022-06-10 17:39:43 +0900

    MapActivity.kt

--- app/src/main/java/com/example/airquality/MapActivity.kt
+++ app/src/main/java/com/example/airquality/MapActivity.kt
@@ -2,10 +2,70 @@ package com.example.airquality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.airquality.databinding.ActivityMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    lateinit var binding: ActivityMapBinding
+
+    private var mMap : GoogleMap ?= null
+    var currentLat = 0.0
+    var currentLng = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        currentLat = intent.getDoubleExtra("currentLat", 0.0)
+        currentLng = intent.getDoubleExtra("currentLng", 0.0)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap : GoogleMap) {
+        mMap = googleMap
+
+        mMap?.let {
+            val currentLocation = LatLng(currentLat, currentLng)
+            // 줌 설정
+            it.setMaxZoomPreference(20.0f)
+            it.setMinZoomPreference(12.0f)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16f))
+        }
+
+        setMarKer()
+
+        binding.fabCurrentLocation.setOnClickListener {
+            val locationProvider = LocationProvider(this@MapActivity)
+            val latitude = locationProvider.getLocationLatitude()
+            val longitude = locationProvider.getLocationLongitude()
+            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 16f))
+
+            setMarKer()
+        }
+    }
+
+    private fun setMarKer() {
+        mMap?.let {
+            it.clear()
+            val markerOptions = MarkerOptions()
+            markerOptions.position(it.cameraPosition.target)
+            markerOptions.title("마커 위치")
+            val marker = it.addMarker(markerOptions)
+
+            it.setOnCameraMoveListener {
+                marker?.let { marker ->
+                    marker.position = it.cameraPosition.target
+                }
+            }
+        }
     }
 }
