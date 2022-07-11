516c8978b3e5bfd7cda9bb0bd13fd348ff91875a
pinocchio22 <vviian.2@gmail.com>
2022-07-11 17:47:38 +0900

    facebook login

--- app/src/main/java/com/p2glet/airquality/LoginActivity.kt
+++ app/src/main/java/com/p2glet/airquality/LoginActivity.kt
@@ -1,28 +1,43 @@
 package com.p2glet.airquality
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.p2glet.airquality.databinding.ActivityLoginBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+
     var googleSignInClient : GoogleSignInClient ?= null
     var GOOGLE_LOGIN_CODE = 9001
     var auth : FirebaseAuth ?= null
+
+    var callbackManager : CallbackManager ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +50,13 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this,gso)
+        callbackManager = CallbackManager.Factory.create()
 
         binding.loginGoogle.setOnClickListener {
             googleLogin()
+        }
+        binding.loginFacebook.setOnClickListener {
+            facebookLogin()
         }
 //        googleLogout()
     }
@@ -53,15 +72,37 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInClient,GOOGLE_LOGIN_CODE)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == GOOGLE_LOGIN_CODE) {
-            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
-            if (result!!.isSuccess) {
-                var account = result.signInAccount
-                firebaseAuthWithGoogle(account)
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    moveMain(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
             }
-        }
     }
 
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
@@ -74,6 +115,18 @@ class LoginActivity : AppCompatActivity() {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode,resultCode,data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
     }
 
 //    fun googleLogout() {
