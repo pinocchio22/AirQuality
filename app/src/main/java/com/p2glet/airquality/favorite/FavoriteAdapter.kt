c8494975a04c8018be0079928c94592e36e7c779
pinocchio22 <vviian.2@gmail.com>
2022-07-13 14:40:58 +0900

    if uid == item.uid

--- app/src/main/java/com/p2glet/airquality/favorite/FavoriteAdapter.kt
+++ app/src/main/java/com/p2glet/airquality/favorite/FavoriteAdapter.kt
@@ -11,6 +11,8 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.setPadding
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.p2glet.airquality.FavoriteActivity
 import com.p2glet.airquality.FavoriteLocation
@@ -25,6 +27,7 @@ import com.p2glet.airquality.R
 class FavoriteAdapter(val itemList: ArrayList<FavoriteItem>): RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
 
     val db = FirebaseFirestore.getInstance()
+    var auth = FirebaseAuth.getInstance()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.favorite_item, parent, false)
@@ -36,51 +39,53 @@ class FavoriteAdapter(val itemList: ArrayList<FavoriteItem>): RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.name.text = itemList[position].name
-        holder.location.text = itemList[position].location
+        if (auth.uid == itemList[position].uid) {
+            holder.name.text = itemList[position].name
+            holder.location.text = itemList[position].location
 
-        if (itemList[position].favorite) {
-            holder.favorite.setBackgroundResource(R.drawable.ic_star_clicked)
-        }
-        holder.itemView.setOnClickListener {
-            var intent = Intent(it.context, FavoriteLocation::class.java)
-            intent.putExtra("bool", itemList[position].favorite)
-            intent.putExtra("latitude", itemList[position].lat)
-            intent.putExtra("longitude", itemList[position].lng)
-            it.context.startActivity(intent)
-        }
-        // 즐겨찾기 수정
-        holder.setting.setOnClickListener {
-            val builder = AlertDialog.Builder(it.context)
-            val tvName = TextView(it.context)
-            tvName.text = "이름"
-            val etName = EditText(it.context)
-            etName.isSingleLine = true
-            val mLayout = LinearLayout(it.context)
-            mLayout.orientation = LinearLayout.VERTICAL
-            mLayout.setPadding(15)
-            mLayout.addView(tvName)
-            mLayout.addView(etName)
-            builder.setView(mLayout)
+            if (itemList[position].favorite) {
+                holder.favorite.setBackgroundResource(R.drawable.ic_star_clicked)
+            }
+            holder.itemView.setOnClickListener {
+                var intent = Intent(it.context, FavoriteLocation::class.java)
+                intent.putExtra("bool", itemList[position].favorite)
+                intent.putExtra("latitude", itemList[position].lat)
+                intent.putExtra("longitude", itemList[position].lng)
+                it.context.startActivity(intent)
+            }
+            // 즐겨찾기 수정
+            holder.setting.setOnClickListener {
+                val builder = AlertDialog.Builder(it.context)
+                val tvName = TextView(it.context)
+                tvName.text = "이름"
+                val etName = EditText(it.context)
+                etName.isSingleLine = true
+                val mLayout = LinearLayout(it.context)
+                mLayout.orientation = LinearLayout.VERTICAL
+                mLayout.setPadding(15)
+                mLayout.addView(tvName)
+                mLayout.addView(etName)
+                builder.setView(mLayout)
 
-            builder.setTitle("이름을 수정하시겠습니까?")
-            builder.setPositiveButton("확인") { dialog, which ->
-                db.collection("Favorite_Place")
-                    .document("${itemList[position].lat}+${itemList[position].lng}")
-                    .update("name", etName.text.toString())
-                    .addOnSuccessListener { success ->
-                        // 성공
-                        (it.context as Activity).finish()
-                        val intent = Intent(it.context, FavoriteActivity::class.java)
-                        it.context.startActivity(intent)
-                    }
-                    .addOnFailureListener { exception ->
-                        Log.w("FavoriteAdapter", "Error update documents: $exception")
-                    }
+                builder.setTitle("이름을 수정하시겠습니까?")
+                builder.setPositiveButton("확인") { dialog, which ->
+                    db.collection("Favorite_Place")
+                        .document("${itemList[position].lat}+${itemList[position].lng}")
+                        .update("name", etName.text.toString())
+                        .addOnSuccessListener { success ->
+                            // 성공
+                            (it.context as Activity).finish()
+                            val intent = Intent(it.context, FavoriteActivity::class.java)
+                            it.context.startActivity(intent)
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.w("FavoriteAdapter", "Error update documents: $exception")
+                        }
+                }
+                builder.setNegativeButton("취소") { dialog, which ->
+                }
+                builder.show()
             }
-            builder.setNegativeButton("취소") { dialog, which ->
-            }
-            builder.show()
         }
     }
 
