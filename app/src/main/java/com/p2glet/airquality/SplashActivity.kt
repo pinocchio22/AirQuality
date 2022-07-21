4d46bb0f293d0a6c88ac9140413a17f09e4eab31
pinocchio22 <vviian.2@gmail.com>
2022-07-21 17:55:56 +0900

    splash anim

--- app/src/main/java/com/p2glet/airquality/SplashActivity.kt
+++ app/src/main/java/com/p2glet/airquality/SplashActivity.kt
@@ -4,23 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val image = findViewById<ImageView>(R.id.splash_image)
+        image.animation
+
+
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
+            overridePendingTransition(R.anim.splash_out_top, R.anim.splash_in_down)
             finish()
         },DURATION)
 
     }
     companion object {
-        private const val DURATION : Long = 3000
+        private const val DURATION : Long = 2000
     }
+
 
     override fun onBackPressed() {
         super.onBackPressed()
